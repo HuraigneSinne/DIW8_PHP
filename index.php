a7a73b9796dd26a0df17892d4e519b7d944190dc
Jonathan <jonathan.dequidt@laposte.net>
2018-02-22 12:05:50 +0100

    Formulaires

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+
+echo '
+<form method="POST" action="contact.php">
+    <label for="id_civilite">Civilite :</label>
+    <select id="id_civilite" name="civilite">
+        <option value="Madame">Madame</option>
+        <option value="Monsieur">Monsieur</option>
+    </select>
+    <label for="id_pseudo">Pseudo :</label>
+    <input type="text" id="id_pseudo" name="pseudo" />
+    <label for="id_contenu">Contenu :</label>
+    <textarea id="id_contenu" name="contenu"></textarea>
+    <label for="id_email">Email :</label>
+    <input type="text" id="id_email" name="email" />
+    <br>
+    <input type="submit"/>
+</form>
+';
+
+
 $prenom = "Jonathan";
 
 echo "<h1>\n";
@@ -53,6 +73,67 @@ switch ($space1 <=> $space2) {
 // jouer avec timestamp
 echo "Début de la requête à : " . date('d/m/Y H:i:s', $_SERVER['REQUEST_TIME']) . "\n<br>";
 
-sleep(15);
+sleep(1);
 
 echo "Fin de la requête à : " . date('d/m/Y H:i:s') . "\n<br>";
+
+
+
+echo "<h2>Parcours d'un tableau</h2>\n";
+$personnes = array(
+    '0' => array(
+        'nom' => 'Dupont',
+        'prenom' => 'Pierre',
+        'email' => 'pierre.d@gmail.com',
+        'telephones' => array(
+            'fixe' => '03 00 00 00 00',
+            'portable' => '06 00 00 00 00'
+        )
+    ),
+    '1' => array(
+        'nom' => 'Dupont',
+        'prenom' => 'Jean',
+        'email' => 'jean.d@gmail.com',
+        'telephones' => array(
+            'fixe' => '03 00 00 00 00',
+            'portable' => '06 00 00 00 00'
+        )
+    ),
+    '2' => array(
+        'nom' => 'Dupont',
+        'prenom' => 'Marie',
+        'email' => 'marie.d@gmail.com',
+    ),
+  );
+
+$a = 5;
+
+echo "Les téléphones fixe des personnes sont :\n";
+echo "<ul>\n";
+foreach($personnes as $a) {
+    echo "  <li>" . $a['prenom'] . " : ";
+    if (isset($a['telephones']))
+        echo $a['telephones']['fixe'];
+    echo "</li>\n";
+}
+echo "</ul>";
+
+echo "<pre>";
+var_dump($a);
+
+
+print_r($personnes);
+echo "</pre>";
+
+foreach($personnes as $personne) {
+    echo "<ul>\n";
+    foreach($personne as $cle_info => $info) {
+        if (!is_array($info))
+            echo "<li>$cle_info : $info</li>\n";
+        else {
+            foreach($info as $cle => $element)
+                echo "<li>$cle : $element</li>\n";
+        }
+    }
+    echo "</ul>\n";
+}
