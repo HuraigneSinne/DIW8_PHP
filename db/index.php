982abd48358709c7bc7758d73824ae2cc350e202
Jonathan <jonathan.dequidt@laposte.net>
2018-02-26 13:20:25 +0100

    database

--- db/index.php
+++ db/index.php
@@ -3,7 +3,7 @@
 define('HOST', 'localhost'); // Domaine ou IP du serveur ou est située la base de données
 define('USER', 'root'); // Nom d'utilisateur autorisé à se connecter à la base
 define('PASS', ''); // Mot de passe de connexion à la base
-define('DB', 'diw82'); // Base de données sur laquelle on va faire les requêtes
+define('DB', 'diw8'); // Base de données sur laquelle on va faire les requêtes
 
 
 // Connexion à la base locale diw8
@@ -17,5 +17,11 @@ try {
 echo "<pre>last code : " . $db->errorCode() . "\n";
 
 var_dump(get_object_vars($db));
+
+$query = $db->query("SELECT * FROM user");
+var_dump($query);
+
+$result = $query->fetch();
+var_dump($result);
 
 echo "</pre>";
