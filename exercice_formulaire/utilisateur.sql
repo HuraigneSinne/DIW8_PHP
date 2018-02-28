354eefa08d4b304a2f58f27b707f9ae9da428ebd
Jonathan <jonathan.dequidt@laposte.net>
2018-02-28 15:14:12 +0100

    data

--- exercice_formulaire/utilisateur.sql
+++ exercice_formulaire/utilisateur.sql
@@ -11,3 +11,21 @@ CREATE TABLE `utilisateur` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `lastname`, `firstname`, `email`, `gender`, `birthdate`, `phone`, `mail_subscribe`, `phone_subscribe`) VALUES
+(1, 'jonathan', 'dequidt', 'jo@deq.com', 'M', '1984-01-03', '', 1, 0),
+(2, 'jonathan', 'dequidt', 'a@b.com', 'M', '2018-02-28', '45', 0, 1),
+(4, 'gffdsghfdgfd', 'jonathan', 'edith.piaf@milord.com', 'F', '2018-01-31', '', 0, 0),
+(5, 'gffdsghfdgfd', 'jonathan', 'dfgdsg@fdfdsq.copm', 'F', '2018-01-31', '', 0, 0),
+(6, 'gffdsghfdgfd', 'jonathan', 'dfgdsg@fdfdsq.cop', 'F', '2018-01-31', '', 0, 0),
+(7, 'gffdsghfdgfd', 'jonathan', 'dfgdsg@fdfdsq.co', 'F', '2018-01-31', '', 0, 0),
+(8, 'gffdsghfdgfd', 'jonathan', 'dfdsg@fdfdsq.co', 'F', '2018-01-31', '', 0, 0),
+(9, 'gffdsghfdgfd', 'jonathan', 'dfdsg@fdfsq.co', 'F', '2018-01-31', '', 0, 0),
+(10, 'gffdsghfdgfd', 'jonathan', 'dfdsg@fdsq.co', 'F', '2018-01-31', '', 0, 0),
+(11, 'gffdsghfdgfd', 'jonathan', 'fdsg@fdsq.co', 'F', '2018-01-31', '', 0, 0),
+(12, 'gffdsghfdgfd', 'jonathan', 'fdsg@fsq.co', 'F', '2018-01-31', '', 0, 0);
