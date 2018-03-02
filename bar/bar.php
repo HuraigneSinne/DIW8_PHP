85d9fd72a94d19cfdc09167cf110594fa6cfecf0
Jonathan <jonathan.dequidt@laposte.net>
2018-03-02 14:17:54 +0100

    Bar fiche

--- bar/bar.php
+++ bar/bar.php
@@ -1,28 +1,31 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulaire</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,700" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-</head>
-<body>
-    <?php
-        // Inclusion du fichier database.php
-        require_once('../lib/database.php');
-        require_once('../lib/fonctions.php');
+<?php
+    $title = "Fiche";
+    require_once('header.php');
 
-        $id_bar = getInt('id');
-    ?>
+    $id_bar = getInt('id');
+    $page_error = [];
 
+    // récupération des infos du bar
+    if ($id_bar != 0) {
+        $db = connexion();
+        $query = $db->prepare("SELECT name, adresse, rating, style FROM bar WHERE id = :id");
+        $query->bindValue(":id", $id_bar, PDO::PARAM_INT);
+        $query->execute();
+        if (!$bar = $query->fetch())
+            $page_error[] = 'Bar inconnu';
+    }
 
-    Fiche du bar N°<?php echo $id_bar; ?>
+    var_dump($page_error);
+?>
+    
+    <main role="main" class="container">
+        <h1>Fiche du bar N°<?php echo $id_bar; ?></h1>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</body>
-</html>
+        <div>
+            
+        </div>
+    </main>
+
+<?php
+    require_once('footer.php');
+?>
