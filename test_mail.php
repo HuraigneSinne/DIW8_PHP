371b7fc3c2048a9fda564c6219cc54390392546f
Jonathan <jonathan.dequidt@laposte.net>
2018-03-02 11:11:34 +0100

    Test email

---
+++ test_mail.php
@@ -0,0 +1,8 @@
+<?php
+$from = "emailtest@YOURDOMAIN";
+$to = "YOUREMAILADDRESS";
+$subject = "PHP Mail Test script";
+$message = "This is a test to check the PHP Mail functionality";
+$headers = "From:" . $from;
+mail($to,$subject,$message, $headers);
+echo "Test email sent";
