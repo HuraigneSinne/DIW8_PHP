92a637fa06ab9e1191105dbf09fb3b412ce819e1
Jonathan <jonathan.dequidt@laposte.net>
2018-03-05 14:16:16 +0100

    Lost password : 2

--- bar/lost_password.php
+++ bar/lost_password.php
@@ -5,11 +5,71 @@
     $errors = [];
     $form_errors = [];
 
+    $db = connexion();
+
     // Si on a un token fourni (étape 2)
+    $token = getValue('token');
+    if ($token != '') {
+        // Vérification du token en base
+        $query = $db->prepare("SELECT id_user, token_valid_until FROM auth_user WHERE token = :token");
+        $query->bindValue(':token', $token, PDO::PARAM_STR);
+        $query->execute();
+
+        // Si le token n'est pas en base affichage d'une page d'interdiction (403)
+        if (!$user = $query->fetch()) {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden !!', true, 403);
+            die("accès refusé");
+        }
+
+        // Calcul de la validité de date du token si invalide affichage d'une page d'interdiction (403)
+        $token_valid_until = new DateTime($user['token_valid_until']);
+        $date = new DateTime('now');    // Date actuelle
+        $date->setTime(0, 0);           // Remise à 0 de l'heure de la date actuelle
+        if ($date > $token_valid_until) {
+            // Suppression des lignes de auth_user concernant cet utilisateur
+            $query = $db->prepare("DELETE FROM auth_user WHERE id_user = :id_user");
+            $query->bindValue(':id_user', $user['id_user'], PDO::PARAM_INT);
+            $query->execute();
+            // Affichage de la page d'interdiction
+            header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden !!', true, 403);
+            die("accès refusé");
+        }
+
+        // A partir d'ici le token est valide, le formulaire de changement du mot de passe peut être affiché
+    }
 
 
     // Si le formulaire de changement du mot de passe est fourni (étape 3)
+    if (postValue('pasword_form') == 1) {
+        // Validation du mot de passe
+        $password = postValue('password');
+        $passwordConfirm = postValue('passwordConfirm');
 
+        if ($password == '') {
+            $form_errors['password'] = "Veuillez saisir un mot de passe";
+        }
+        if ($passwordConfirm == '') {
+            $form_errors['passwordConfirm'] = "Veuillez saisir un mot de passe";
+        } elseif ($password != $passwordConfirm) {
+            $form_errors['password'] = "Mot de passe différent !";
+        }
+
+        if (count($form_errors) == 0) {
+            // Modification du mot de passe de l'utilisateur
+            $query = $db->prepare("UPDATE `user` SET password = :password WHERE id = :id");
+            $query->bindValue(':password', password_hash($password, PASSWORD_DEFAULT), PDO::PARAM_STR);
+            $query->bindValue(':id', $user['id_user'], PDO::PARAM_INT);
+            $query->execute();
+
+            // Suppression des lignes de auth_user concernant cet utilisateur
+            $query = $db->prepare("DELETE FROM auth_user WHERE id_user = :id_user");
+            $query->bindValue(':id_user', $user['id_user'], PDO::PARAM_INT);
+            $query->execute();
+
+            // Connexion de l'utilisateur
+            redirect('index.php');
+        }
+    }
 
     // Si le formulaire d'oubli du mot de passe est fourni (étape 1)
     if (postValue('forgot_form') == 1) {
@@ -23,7 +83,6 @@
 
         if (count($form_errors) == 0) {
             // Vérification de la présence de l'email dans la table user
-            $db = connexion();
             $query = $db->prepare("SELECT id FROM user WHERE email = :email");
             $query->bindValue(':email', $email, PDO::PARAM_STR);
             $query->execute();
@@ -72,7 +131,8 @@ Si vous n'êtes pas à l'origine de cette demande, merci d'ignorer ce message.
 ?>
 
 <div class="container">
-    <?php if (!isset($url)) : ?>
+    <?php if (empty($token)) :  // Si le token est vide (étape 1)?>
+
     <h2>Veuillez saisir votre adresse email</h2>
     <form method="post" class="form">
         <input type="hidden" name="forgot_form" value="1"/>
@@ -84,8 +144,28 @@ Si vous n'êtes pas à l'origine de cette demande, merci d'ignorer ce message.
         <button class="btn btn-lg btn-primary btn-block">Valider</button>
     </form>
 
-    <?php else : ?>
-    <span>Vous allez recevoir un email avec un lien pour pouvoir modifier votre mot de passe</span>
+    <?php elseif (isset($url)) : // fin de l'étape 1 après création de l'url ?>
+
+    <span>Vous allez recevoir un email avec un lien pour pouvoir modifier votre mot de passe</span>    
+
+    <?php else : // Sinon (étape 2) ?>
+
+    <h2>Veuillez saisir votre nouveau mot de passe</h2>
+    <form method="post" class="form">
+        <input type="hidden" name="pasword_form" value="1"/>
+        <div class="form-label-group">
+            <input type="password" class="form-control <?php echo isset($form_errors['password']) ? 'is-invalid' : '' ?>" name="password" id="password" placeholder="Votre mot de passe" required autofocus/>
+            <label for="password">Votre mot de passe</label>
+            <?php echo isset($form_errors['password']) ? '<div class="invalid-feedback">' . $form_errors['password'] . '</div>' : '' ?>
+        </div>
+        <div class="form-label-group">
+            <input type="password" class="form-control <?php echo isset($form_errors['passwordConfirm']) ? 'is-invalid' : '' ?>" name="passwordConfirm" id="passwordConfirm" placeholder="Confirmez" required autofocus/>
+            <label for="passwordConfirm">Confirmez</label>
+            <?php echo isset($form_errors['passwordConfirm']) ? '<div class="invalid-feedback">' . $form_errors['passwordConfirm'] . '</div>' : '' ?>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block">Valider</button>
+    </form>
+
     <?php endif; ?>
 
 </div>
